5b97389ad372369467a9917d2f8321aa24ee12a2
Zsolt Gál <zsolt.gal@sessiondigital.com>
2013-07-30 16:30:57 +0200

    Renamed recreate_database.sh to recreate_db.sh

--- recreate_db.sh
+++ recreate_db.sh
@@ -2,27 +2,31 @@
 
 SQL=$1;
 DATABASE=$2;
-if ["$SQL" = ""]; then
-    SQL=$DATABASE-latest.sql
-fi
-
-if [!-f $SQL]; then
-    echo "Dump file $SQL not exists!"
-    exit 1
-fi
-
 if ["$DATABASE" = ""]; then
     DATABASE='magentodb'
 fi
 
-mysqladmin -uroot -proot drop $DATABASE
-if mysqladmin -uroot -proot create $DATABASE; then
-    echo "Database \"$DATABASE\" created"
-else
-    exit "Cannot create database $DATABASE"
+if ["$SQL" = ""]; then
+    SQL=$DATABASE-latest.sql
 fi
-if mysql -uroot -proot $DATABASE < $SQL; then 
-    echo "Database \"$DATABASE\" imported"
+
+if [ ! -f $SQL ]; then
+    echo "Dump file $SQL not exists!"
+    exit 1
+fi
+
+if mysqladmin -uroot -proot drop $DATABASE; then
+
+    if mysqladmin -uroot -proot create $DATABASE; then
+        echo "Database \"$DATABASE\" created"
+    else
+        exit "Cannot create database $DATABASE"
+    fi
+    if mysql -uroot -proot $DATABASE < $SQL; then 
+        echo "Database \"$DATABASE\" imported"
+    else
+        echo "Data import to $DATABASE failed from file $SQL"
+    fi
 else
-    echo "Data import to $DATABASE failed from file $SQL"
+    echo "Nothing to do"
 fi
