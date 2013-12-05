b91eadf165bed19f7a29b2470ec36aa613fea2e4
Zsolt Gál <zsolt.gal@sessiondigital.com>
2013-12-05 11:20:25 +0100

    Fix wrong SQL in increase admin session

--- increase-admin-session.sh
+++ increase-admin-session.sh
@@ -1,6 +1,6 @@
 echo Increase admin session lifetime to 86400, ignoring lockout failures
 echo "INSERT INTO core_config_data (scope, scope_id, path, value) VALUES" > /tmp/increase-admin-session.sql
 echo "('default', 0, 'admin/security/session_cookie_lifetime', 86400)" >> /tmp/increase-admin-session.sql
-echo "ON DUPLICATE KEY UPDATE value = 86400 WHERE path = 'admin/security/session_cookie_lifetime';" >> /tmp/increase-admin-session.sql
+echo "ON DUPLICATE KEY UPDATE value = 86400;" >> /tmp/increase-admin-session.sql
 mysql -uroot -proot magentodb < /tmp/increase-admin-session.sql
 rm /tmp/increase-admin-session.sql
